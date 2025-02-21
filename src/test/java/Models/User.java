4586b6e95f304c8a77ad2ddff5d9c76b457007ae
johnn <johnnid@mail.ru>
2025-02-21 21:34:27 +0300

    Update gitignore

---
+++ src/test/java/Models/User.java
@@ -0,0 +1,21 @@
+package Models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+
+@Data
+@AllArgsConstructor
+@Builder
+
+public class User {
+    private Integer id;
+    private String username;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String phone;
+    private Integer userStatus;
+}
